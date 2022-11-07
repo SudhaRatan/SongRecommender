47133dfe0d332d145b2cd5541261f9f903034a1b
SudhaRatan <rattankumar2511@gmail.com>
2022-11-07 20:25:46 +0530

    Init

--- server.js
+++ server.js
@@ -28,4 +28,6 @@ router
 
 
 app.use('/',router)
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
